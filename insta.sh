937b8da5555788d6f1089dcc7a9dc3edc4599732
AliceInWonderland100 <WonderlandAlice100@gmail.com>
2017-10-23 00:20:48 -0400

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -1,1 +1,9 @@
 $ npm install --save get-instagram-photo
+
+const getInstagramPhoto = require('get-instagram-photo')
+
+getInstagramPhoto('https://www.instagram.com/p/BItduURBUpF/')
+  .then(image => {
+    console.log(image)
+    //=> https://scontent.cdninstagram.com/t51.2885-15/e35/13735878_229794197415635_1137269208_n.jpg?ig_cache_key=MTMwOTgzMzc5MjgzOTgzMDA4NQ%3D%3D.2
+  })
